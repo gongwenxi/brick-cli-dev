7d1d78baef0f9e388c75c596f68173357c6734ba
Wenxi <>
2023-11-28 11:48:58 +1300

    remove bin folder

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 //const utils = require("@brick-cli-dev/utils");
 //console.log('hello block-cli');
 //console.log(utils());
@@ -7,5 +7,5 @@ if(importLocal(__filename)){
     require('npmlog').info('cli', 'using local version of brick-cli-dev');
 }
 else{
-    require('../lib/core.js')(process.argv.slice(2));
+    require('./lib/core.js')(process.argv.slice(2));
 }
